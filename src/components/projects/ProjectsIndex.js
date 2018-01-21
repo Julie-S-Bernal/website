b29ede748fc28943cf752688f67b514c78a16782
Julie-S-Bernal <julies.bernal@gmail.com>
2018-01-21 23:41:23 +0000

    added images

--- src/components/projects/ProjectsIndex.js
+++ src/components/projects/ProjectsIndex.js
@@ -109,19 +109,22 @@ class ProjectsIndex extends React.Component {
                   <h2 className="title">
                     Game -What the Hell?
                   </h2>
-                  <div className="image_container">
+                  <div className="game_container">
                   <img className="image" src={project1}></img>
                   </div>
                   <div className="tech-container">
                   <h4 className="tech">jQuery | CSS3 | HTML5 | Babel | Webpack </h4>
                   </div>
                    <div className="description">
+                   <div className="t-align">
                     <p className="description_text">
                     What the hell is an endless flyer game. This app features collisions, key binding, timers, scores and sounds.
                     </p>
-                    <Button raised color="accent"><a className="links"  href="https://nameless-cliffs-17077.herokuapp.com"> Heroku </a></Button>
-                    <Button raised color="accent"><a className="links"  href="https://github.com/Julie-S-Bernal/WDI_PROJECT_1"> GitHub </a></Button>
-
+                    </div>
+                    <div className="b-align">
+                    <Button className="link-button" raised color="accent"> <a className="button-name"  href="https://nameless-cliffs-17077.herokuapp.com"> Heroku </a></Button>
+                    <Button className="link-button" raised color="accent"> <a className="button-name"  href="https://github.com/Julie-S-Bernal/WDI_PROJECT_1"> GitHub </a></Button>
+                    </div>
                   </div>
                 </Grid>
              
@@ -130,7 +133,7 @@ class ProjectsIndex extends React.Component {
                     <h2 className="title">
                     Wesite- TeaMood
                     </h2>
-                    <div className="image_container">
+                    <div className="website_container">
                     <img className="image" src={project2}></img>
                   </div>
                   <div className="tech-container">
@@ -141,9 +144,10 @@ class ProjectsIndex extends React.Component {
                      TeaMood is an app that put tea lovers in contact so that they can find together new teas to try.
                      This app features login, user authentication, creation of posts, comments and error handlers.S
                      </p>
-                     <Button raised color="accent"><a className="links" href="https://project-2-teas.herokuapp.com/register"> Heroku </a></Button>
-                     <Button raised color="accent"><a className="links" href="https://github.com/Julie-S-Bernal/WDI_PROJECT_2"> GitHub</a></Button>
-
+                     <div className="b-align">
+                     <Button className="link-button" raised color="accent"><a className="button-name" href="https://project-2-teas.herokuapp.com/register"> Heroku </a></Button>
+                     <Button className="link-button" raised color="accent"><a className="button-name" href="https://github.com/Julie-S-Bernal/WDI_PROJECT_2"> GitHub</a></Button>
+                     </div>
                   </div>
                 </Grid>
                 <Grid item xs={12} sm={6} md={6} lg={6} xl={3}>
@@ -151,17 +155,20 @@ class ProjectsIndex extends React.Component {
                     <h2 className="title">
                     App -ExploreMore
                     </h2>
-                    <div className="image_container">S
+                    <div className="app_container">
                     <img className="image" src={project3}></img>
                     </div>
+                    <div className="tech-container">
                       <h4 className="tech"> MEAN | Bulma | HTML5 | CSS3 |  Googlemap API  | Mocha | Chai | Istambul | Bcrypt | Bluebird | Expres | Express-jwt | Mongoose| MongoDB  | Babel | Bower| Cors | Gulp | Husky | Supertest | Morgan | Istambul </h4>
+                    </div>
                       <div className="description">
                     <p className="description_text">
                      A MEAN stack application done as a group. We managed to work well as a team by doing pair programming. I coded alone errors handlers, the menu animation, part of the front-end and the styling of the maps.
                      </p>
-                     <Button raised color="accent"><a className="links"  href="https://exploremore-julie.herokuapp.com/"> Heroku </a></Button>
-                     <Button raised color="accent"><a className="links"  href="https://github.com/JacieWDI/WDI_GROUP_PROJECT"> GitHub </a></Button>
-
+                     <div className="b-align">
+                     <Button className="link-button" raised color="accent"><a className="button-name"  href="https://exploremore-julie.herokuapp.com/"> Heroku </a></Button>
+                     <Button className="link-button" raised color="accent"><a className="button-name" href="https://github.com/JacieWDI/WDI_GROUP_PROJECT"> GitHub </a></Button>
+                     </div>
                 </div>
                 </Grid>
                 <Grid item xs={12} sm={6} md={6} lg={6} xl={3}>
@@ -169,18 +176,23 @@ class ProjectsIndex extends React.Component {
                     <h2 className="title">
                     Phone App- Spare
                     </h2>
-                    <div className="image_container">
+                    <div className="phone_container">
                     <img className="image" src={project4}></img>
                     </div>
-
+                    <div className="tech-container">
                     <h4 className="tech"> MERN | Material-ui| HTML5 | Webpack | CSS3 |  API  | Axios | Bcrypt | Bluebird | Express | JSOn web token| React-dom | Mongoose | MongoDB | Morgan  | Babel | Chai | Mocha | Enzyme | Istambul | Supertest | Sinon | Victory | Moment.js</h4>
+                    </div>
                     <div className="description">
                     <p className="description_text">
                       Spare is an app that help the user save money by helping him plan his budget and giving in real time the exchange rates. The user spendings are also recorded and displayed into graphs so he/she can know exactly how much or less he/she has spend in his/her travel.
                      This app features an external API, login, registration, authentication, tests, error-handlers, calendars, pickers, real-time data, forms.
                      </p>
-                     <Button raised color="accent"><a className="links" href="https://frozen-cliffs-27123.herokuapp.com/login"> Heroku </a></Button>
-                     <Button raised color="accent"><a className="links"  href="https://github.com/Julie-S-Bernal/WDI_PROJECT_4"> GitHub </a></Button>
+                     <div className="b-align">
+                     <Button className="link-button" raised color="accent"> <a className="button-name" href="https://frozen-cliffs-27123.herokuapp.com/login"> Heroku </a></Button>
+                     <Button className="link-button" raised color="accent"> <a className="button-name"  href="https://github.com/Julie-S-Bernal/WDI_PROJECT_4"> GitHub </a></Button>
+                     </div>
+
+                 
 
                   </div>
                 </Grid>
